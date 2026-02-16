fc925b25bb957aae98adf8f8d88f6b31a08995a7
sagardigitalk <sagardigitalk@gmail.com>
2026-02-16 16:00:06 +0530

    uplaode

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 const router = express.Router();
+
+const uploadDir = 'uploads/';
+
+// Create uploads folder if not exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, uploadDir);
   },
   filename(req, file, cb) {
     cb(
